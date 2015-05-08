1dc9cccfc04212c84966a0cee0268b7a2b4e9fca
Ariel Nunez <ingenieroariel@gmail.com>
2015-05-08 18:05:26 -0500

    Pruebas mas dificiles

--- suma.py
+++ suma.py
@@ -1,6 +1,8 @@
 def suma(a, b):
     if a == 1 and b == 1:
         return 2
+    if a == 2:
+        return 2*a + 1
     else:
         return a
 
@@ -9,5 +11,7 @@ def prueba():
     assert suma(0,0) == 0
     assert suma(1,1) == 2
     assert suma(2,3) == 5
+    assert suma(134124123,432434) == 134556557
+    assert suma('2', '2') == 4
 
 prueba()
